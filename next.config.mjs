c4d8c915cdd882a6f217fc4e5aa7e48af3570603
ashraf-walid <173446507+ashraf-walid@users.noreply.github.com>
2025-12-13 07:22:17 +0200

    Change config from Webpack to Turbopack

--- next.config.mjs
+++ next.config.mjs
@@ -93,34 +93,35 @@ const nextConfig = {
     ],
     webpackBuildWorker: true,
   },
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      config.optimization = {
-        ...config.optimization,
-        splitChunks: {
-          chunks: "all",
-          minSize: 20000,
-          maxSize: 244000,
-          minChunks: 1,
-          maxAsyncRequests: 30,
-          maxInitialRequests: 30,
-          cacheGroups: {
-            defaultVendors: {
-              test: /[\\/]node_modules[\\/]/,
-              priority: -10,
-              reuseExistingChunk: true,
-            },
-            default: {
-              minChunks: 2,
-              priority: -20,
-              reuseExistingChunk: true,
-            },
-          },
-        },
-      };
-    }
-    return config;
-  },
+  turbopack: {},
+  // webpack: (config, { dev, isServer }) => {
+  //   if (!dev && !isServer) {
+  //     config.optimization = {
+  //       ...config.optimization,
+  //       splitChunks: {
+  //         chunks: "all",
+  //         minSize: 20000,
+  //         maxSize: 244000,
+  //         minChunks: 1,
+  //         maxAsyncRequests: 30,
+  //         maxInitialRequests: 30,
+  //         cacheGroups: {
+  //           defaultVendors: {
+  //             test: /[\\/]node_modules[\\/]/,
+  //             priority: -10,
+  //             reuseExistingChunk: true,
+  //           },
+  //           default: {
+  //             minChunks: 2,
+  //             priority: -20,
+  //             reuseExistingChunk: true,
+  //           },
+  //         },
+  //       },
+  //     };
+  //   }
+  //   return config;
+  // },
 };
 
 export default nextConfig;
