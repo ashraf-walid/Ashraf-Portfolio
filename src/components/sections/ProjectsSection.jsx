ec84c55dd7b2cb5d7555238853c8ca2692a72982
ashraf-walid <ashrafelgezery2014@gmail.com>
2026-07-29 10:49:39 +0300

    project 5

--- src/components/sections/ProjectsSection.jsx
+++ src/components/sections/ProjectsSection.jsx
@@ -304,7 +304,7 @@ export default function ProjectsSection() {
                           href={project.adminPanel}
                           target="_blank"
                           rel="noopener noreferrer"
-                          className="flex items-center justify-center gap-2 px-4 py-3 bg-gradient-to-r from-purple-500 to-blue-500 text-white font-semibold rounded-xl hover:from-purple-600 hover:to-blue-600 focus:ring-2 focus:ring-purple-400 focus:outline-none duration-500 transition flex-1 shadow-lg"
+                          className="flex items-center justify-center gap-2 px-4 py-3 bg-gradient-to-r from-blue-600 to-blue-500 text-white font-semibold rounded-xl hover:from-purple-600 hover:to-blue-600 focus:ring-2 focus:ring-purple-400 focus:outline-none duration-500 transition flex-1 shadow-lg"
                         >
                           <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
